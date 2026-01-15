f928e07dbc36a3b782c1dbb2ea381813a0f4c5e1
Rafal <r.morawski92@gmail.com>
2026-01-15 13:31:10 +0100

    first commit

---
+++ cypress/e2e/example.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+before("open home page ", () => {
+    cy.visit("")
+})
+describe("koszyk", () => {
+    it("dodawanie do koszyka", () => {
+    })
+
+    it("proces niepoprawnego zakupu na stronie", () =>{
+
+    })
+
+})
